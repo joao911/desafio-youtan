cb69e7f9337e5f0f57b8b1cec487ee10d2bfdf57
Joao Paulo <joaopaulolacerda911@gmail.com>
2024-10-13 02:29:17 -0300

    feat: start test

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
+///<reference types="vite/client" />
+///<reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { InlineConfig } from "vitest";
-import { UserConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    globals: true,
-  },
-} as UserConfig & InlineConfig);
+});
