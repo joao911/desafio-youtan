d57162494db1ebab5519217a2ec7175215d56e5e
Joao Paulo <joaopaulolacerda911@gmail.com>
2024-10-17 09:55:05 -0300

    feat: added delay

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,3 +3,10 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:3000/",
 });
+
+api.interceptors.request.use(async (config) => {
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.round(Math.random() * 1000))
+  );
+  return config;
+});
