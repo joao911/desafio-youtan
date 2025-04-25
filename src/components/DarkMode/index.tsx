94e5632d245ec29a92840cfaf0787b6533563f74
joao911 <joaopaulolacerda911@gmail.com>
2025-04-24 23:28:04 -0300

    feat: added test

--- src/components/DarkMode/index.tsx
+++ src/components/DarkMode/index.tsx
@@ -1,16 +1,19 @@
 import { useTheme } from "@/hooks/useTheme";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { Tooltip } from "@mui/material";
 
 export function DarkMode() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="p-2 text-gray-800 transition-all duration-300 bg-gray-200 rounded-lg dark:bg-gray-800 dark:text-gray-200"
-    >
-      {theme === "dark" ? <DarkModeIcon /> : <Brightness4Icon />}
-    </button>
+    <Tooltip title={theme === "dark" ? "Tema escuro" : "Tema claro"}>
+      <button
+        onClick={toggleTheme}
+        className="p-2 text-gray-800 transition-all duration-300 bg-gray-200 rounded-lg dark:bg-gray-800 dark:text-gray-200"
+      >
+        {theme === "dark" ? <DarkModeIcon /> : <Brightness4Icon />}
+      </button>
+    </Tooltip>
   );
 }
