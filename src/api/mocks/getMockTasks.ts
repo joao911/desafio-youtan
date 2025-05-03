b92c6f53e5f2bf9e2d50b4fda08b3296230e94e0
joao911 <joaopaulolacerda911@gmail.com>
2025-05-03 01:55:06 -0300

    feat: adjusted mock

--- src/api/mocks/getMockTasks.ts
+++ src/api/mocks/getMockTasks.ts
@@ -1,9 +1,30 @@
 import { http, HttpResponse, HttpHandler } from "msw";
 
-export const getMockTask: HttpHandler = http.get("/tasks", () => {
+export const getMockTask: HttpHandler = http.get("/tasks", ({ request }) => {
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get('_page')) || 1;
+  const perPage = Number(url.searchParams.get('_per_page')) || 8;
+
   return HttpResponse.json({
-    data: {
-      data: [],
-    },
+    first: 1,
+    prev: null,
+    next: null,
+    last: 1,
+    pages: 1,
+    items: 2,
+    data: [
+      {
+        "title": "Verificar listagem de carnes com parcelas a pagar (user normal - regra so pode listar carnês que tem parcelas de 1° até a 12° parcela)",
+        "status": "done",
+        "id": "24522f97-39f9-44e2-8208-05bb682b40d2",
+        "createdAt": "2025-05-03T02:52:52.282Z"
+      },
+      {
+        "title": "Verificar listagem de carnes disponíveis a resgate (Deve conter apenas os carnês com Status quitado e opcional pagar 13° parcela)",
+        "status": "done",
+        "id": "b3de1778-dc9e-4acd-aa33-9cc797b5ed8d",
+        "createdAt": "2025-05-03T03:14:03.346Z"
+      }
+    ]
   });
 });
