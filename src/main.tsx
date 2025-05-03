3d509d4174ab0c0c661f1d7b004a50614098b4f8
Joao Paulo <joaopaulolacerda911@gmail.com>
2025-05-03 02:14:01 -0300

    feat: added get tasks

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
-
+import { enableMSW } from "./api/mocks/index.ts";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+enableMSW().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
